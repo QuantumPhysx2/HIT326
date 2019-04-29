7dc4ed97ad923dc009e7ceceb7c6953bd4ef6036
Gia <33999927+QuantumPhysx2@users.noreply.github.com>
2019-04-29 19:03:48 +0930

    Adding register page
    - Not fully functioning properly

---
+++ assets/php/db.php
@@ -0,0 +1,20 @@
+<?php
+// Database connection settings (change for Spinetail)
+$dbAdapter = "mysql";
+$dbHost = "localhost";
+$dbPort = 3307;
+$dbName = "hit326";
+$dbUsername = "root";
+$dbPassword = "@BigBaus123";
+
+// Attempt connection to database
+try {
+    // Define variable to create a new connection (keep an eye on spelling)
+    $conn = new PDO("$dbAdapter:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUsername, $dbPassword);
+    // Setup error checking attributes
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $err) {
+    // Should only occur if the connection parameters are incorrect
+    die("Connection error. Check the configurations".$err->getMessage());
+}
+?>
