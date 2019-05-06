5ffda67be2cc677db6c617c40d44d3034728b75d
JerryLee <leechiyin1990@gmail.com>
2019-05-06 11:58:47 +0930

    Bootstrap

--- index.php
+++ index.php
@@ -118,33 +118,50 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <meta name="description" content="HIT326">
         <meta name="keywords" content="Products">
         <meta name="theme-color" content="#222">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="assets/css/style.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
         <link rel="icon" href="assets/images/page-icon.ico">
         <title>Home | PC Buy Here</title>
     </head>
 
     <body>
         <header>
-            <div class="navigation">
-                <div class="jumbo-1">
-                    <a class="home" href="index.php"><img src="assets/images/page-logo.png" alt="PC Buy Here"></a>
-                </div>
-                <div class="jumbo-2">
-                    <!-- Refer to: https://www.w3schools.com/icons/fontawesome_icons_webapp.asp -->
-                    <a class="nav-child" href="index.php"><i class="fa fa-cart-plus" title="Marketplace"></i></a>
-                    <a class="nav-child" href="#"><i class="fa fa-bell-o" title="Icon 2"></i></a>
-                    <a class="nav-child" href="#"><i class="fa fa-bell-o" title="Icon 3"></i></a>
-                    <a class="nav-child" href="#"><i class="fa fa-bell-o" title="Icon 4"></i></a>
-                    <a class="nav-child" href="#"><i class="fa fa-bell-o" title="Icon 5"></i></a>
-                    <a class="nav-child" href="login.php"><i class="fa fa-user" title="Login"></i></a>
-                </div>
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.php"><i class="fas fa-desktop"></i> PC Buy Here</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav  mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#"><i class="fa fa-cart-plus" title="Marketplace"></i></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#"><i class="fa fa-bell-o" title="Icon 2"></i></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#"><i class="fa fa-bell-o" title="Icon 3"></i></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#"><i class="fa fa-bell-o" title="Icon 4"></i></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#"><i class="fa fa-bell-o" title="Icon 5"></i></a>
+                </li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0" action="search.php" method="GET">
+                 <input class="form-control mr-sm-2" type="text" id="search" name="search" />
+                 <select class="custom-select custom-select-sm mr-2" name="category">
+                    <option value="">Choose catagory</option>
+                    <option value="Graphics">GPU</option>
+                    <option value="RAM">RAM</option>
+                 </select>
+                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
             </div>
+          </nav>
         </header>
 
         <main>
-            <div class="container">
+            <div class="container mt-4">
                 <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
                     <input type="text" name="inp_USERNAME" placeholder="Username" required>
                     <input type="password" name="inp_PASSWORD" placeholder="Password" required>
@@ -176,8 +193,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         <footer></footer>
 
-        <script src="https://www.kryogenix.org/code/browser/sorttable/sorttable.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.min.js"></script>
-        <script src="assets/js/main.js"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
     </body>
 </html>
